e70156ef290406c1d9c089a08fa0fc54ca075191
Jo Elder <jo@isthebest.com>
2017-02-08 16:18:14 +0000

    Creating CleaningProductTest class

---
+++ specs/CleaningProductTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import supermarket.models.*;
+
+public class CleaningProductTest {
+
+	CleaningProduct bleach = new CleaningProduct("bleach", 3.14, 5.11);
+	CleaningProduct rubberGloves = new CleaningProduct("rubber gloves", 0.89, 3.50);
+
+
+}
