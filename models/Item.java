d3925cbfb9a98ded54f096b07ea37972841f8db0
Jo Elder <jo@isthebest.com>
2017-02-08 16:03:42 +0000

    Writing Item class

---
+++ models/Item.java
@@ -0,0 +1,14 @@
+
+package supermarket.models;
+
+public abstract class Item {
+
+	boolean threeForTwo;
+	int fixedPriceDiscountQuantity;
+	float fixedPrice;
+	int bulkMinQuantity;
+	int bulkDiscountQuantity;
+	float bulkDiscount;
+
+
+}
