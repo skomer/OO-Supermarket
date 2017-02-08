e9b2da765a71a10c8ba078b41af8b454e6948fa6
Jo Elder <jo@isthebest.com>
2017-02-08 16:27:50 +0000

    Writing CleaningProduct constructor test

--- specs/CleaningProductTest.java
+++ specs/CleaningProductTest.java
@@ -7,5 +7,10 @@ public class CleaningProductTest {
 	CleaningProduct bleach = new CleaningProduct("bleach", 3.14, 5.11);
 	CleaningProduct rubberGloves = new CleaningProduct("rubber gloves", 0.89, 3.50);
 
+	@Test
+	public void hasName() {
+		assertEquals("bleach", bleach.getName());
+	}
+
 
 }
