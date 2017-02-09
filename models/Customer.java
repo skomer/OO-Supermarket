a2b6d762ffcbe8d31051803550ea25af0bb944ad
Jo Elder <jo@isthebest.com>
2017-02-09 14:41:38 +0000

    Creating Customer class and CustomerType enum

---
+++ models/Customer.java
@@ -0,0 +1,19 @@
+
+package models;
+import constants.*;
+
+public class Customer {
+
+	CustomerType customerType;
+	String name;
+
+	public Customer(CustomerType customerType, String name) {
+		this.customerType = customerType;
+		this.name = name;
+	}
+
+
+
+
+
+}
